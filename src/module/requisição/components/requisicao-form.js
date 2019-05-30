6d61bd2cdbcd1f879fe27e07c0c4428a5069e1c0
Gabriel Morandim <augustomorandim@gmail.com>
2019-05-30 12:19:33 -0300

    atualizando form de requisicao

--- src/module/requisição/components/requisicao-form.js
+++ src/module/requisição/components/requisicao-form.js
@@ -1,43 +1,50 @@
 import React from "react";
-import { FormControl, Input, InputLabel } from "@material-ui/core";
+import { FormControl, Input, InputLabel, Grid } from "@material-ui/core";
 
 class RequisicaoForm extends React.Component {
-
-
-render() {
-  return(
-    <form>
-      <FormControl margin="normal" require fullWidth>
-        <InputLabel htmlFor="nome">Nome</InputLabel>
-        <Input
-        id="nome"
-        name="nome"
-        autoComplete="nome"
-        onChange={this.handleNome}
-        />
-      </FormControl>
-      <Grid xs={12}>
-        <FormControl margin="nomral" style={{marginRight: 25}}>
-          <InputLabel htmlFor="data">Data</InputLabel>
+  render() {
+    return (
+      <form>
+        <FormControl margin="normal" require fullWidth>
+          <InputLabel htmlFor="nome">Nome</InputLabel>
           <Input
-          id="data"
-          name="data"
-          autoComplete="data"
-          onChange={this.handleData}
+            id="nome"
+            name="nome"
+            autoComplete="nome"
+            onChange={this.handleNome}
           />
         </FormControl>
+        <Grid xs={12}>
+          <FormControl margin="nomral" style={{ marginRight: 25 }}>
+            <InputLabel htmlFor="data">Data</InputLabel>
+            <Input
+              id="data"
+              name="data"
+              autoComplete="data"
+              onChange={this.handleData}
+            />
+          </FormControl>
+        </Grid>
         <FormControl margin="normal">
           <InputLabel htmlFor="cpf">CPF</InputLabel>
-            <Input
+          <Input
             id="cpf"
             name="cpf"
             autoComplete="cpf"
             onChange={this.handleCpf}
-            />
-          </InputLabel>
+          />
         </FormControl>
-      </Grid>
-    </form>
-  )
+        <FormControl margin="nomral" style={{ marginRight: 25 }}>
+          <InputLabel htmlFor="data">Valor Salario</InputLabel>
+          <Input
+            id="data"
+            name="data"
+            autoComplete="data"
+            onChange={this.handleData}
+          />
+        </FormControl>
+      </form>
+    );
+  }
 }
 export default RequisicaoForm;
