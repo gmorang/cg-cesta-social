1006d8bf9a86d3572d06b9e43fb62003e1b115a9
Felipe <felipefernandesyarroz@gmail.com>
2019-06-03 17:14:58 -0300

    tela de requição faltando o botão de "Enviar Solicitação" tentei ate cansar e nao consegui por

--- src/module/requisição/components/requisicao-form.js
+++ src/module/requisição/components/requisicao-form.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { FormControl, Input, InputLabel, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Fab from "@material-ui/core/Fab";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
+import DeleteIcon from "@material-ui/icons/Delete";
+import NavigationIcon from "@material-ui/icons/Navigation";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 
 class RequisicaoForm extends React.Component {
   render() {
@@ -35,16 +43,66 @@ class RequisicaoForm extends React.Component {
           />
         </FormControl>
         <FormControl margin="nomral" style={{ marginRight: 25 }}>
-          <InputLabel htmlFor="data">Valor Salario</InputLabel>
+          <InputLabel htmlFor="renda">Renda</InputLabel>
           <Input
-            id="data"
-            name="data"
-            autoComplete="data"
-            onChange={this.handleData}
+            id="renda"
+            name="renda"
+            autoComplete="renda"
+            onChange={this.handleRenda}
+          />
+        </FormControl>
+        <FormControl margin="nomral" style={{ marginRight: 25 }}>
+          <InputLabel htmlFor="dependentes">Dependentes</InputLabel>
+          <Input
+            id="dependentes"
+            name="dependentes"
+            autoComplete="dependentes"
+            onChange={this.handleDependentes}
+          />
+        </FormControl>
+        <FormControl margin="nomral" require fullWidth>
+          <InputLabel htmlFor="ong">ONG</InputLabel>
+          <Input
+            id="ong"
+            name="ong"
+            autoComplete="ong"
+            onChange={this.handleONG}
+          />
+        </FormControl>
+        <FormControl margin="nomral" style={{ marginRight: 25 }}>
+          <InputLabel htmlFor="atividade">Atividade</InputLabel>
+          <Input
+            id="atividade"
+            name="atividade"
+            autoComplete="atividade"
+            onChange={this.handleAtividade}
+          />
+        </FormControl>
+        <FormControl margin="nomral" style={{ marginRight: 25 }}>
+          <InputLabel htmlFor="situação">Situação</InputLabel>
+          <Input
+            id="situação"
+            name="situação"
+            autoComplete="situação"
+            onChange={this.handleSituação}
+          />
+        </FormControl>
+        <FormControl margin="nomral" style={{ marginRight: 25 }}>
+          <InputLabel htmlFor="rg">RG</InputLabel>
+          <Input id="rg" name="rg" autoComplete="rg" onChange={this.handleRg} />
+        </FormControl>
+        <FormControl margin="nomral" style={{ marginRight: 25 }}>
+          <InputLabel htmlFor="status">Status</InputLabel>
+          <Input
+            id="status"
+            name="status"
+            autoComplete="status"
+            onChange={this.handleTipodecesta}
           />
         </FormControl>
       </form>
     );
   }
 }
+
 export default RequisicaoForm;
