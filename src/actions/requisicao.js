980d664040e13b73e73ca771eac7565924e76d73
Gabriel Morandim <augustomorandim@gmail.com>
2019-06-13 00:34:41 -0300

    função para listar requisicoes

--- src/actions/requisicao.js
+++ src/actions/requisicao.js
@@ -1,12 +1,27 @@
-import { firestore } from "../config/firebase";
+import { firestore, firebase } from "../config/firebase";
 
 export const criaRequisicao = requisicao => {
   firestore
     .collection("requisicao")
     .doc()
-    .set(requisicao)
+    .set({ requisicao, user: firebase.auth().currentUser.uid })
     .then(response => {
       console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+export const listaRequisicao = () => {
+  return firestore
+    .collection("requisicao")
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        console.log(doc.data());
+        doc.data();
+      });
     })
     .catch(err => {
       console.log(err);
